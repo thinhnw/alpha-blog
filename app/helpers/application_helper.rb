25c6f5de0f7f6bad68a512c645fe8b3c00c90cd9
thinhnw <thinhnv0412@gmail.com>
2022-06-19 14:32:11 +0000

    Add users show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def  gravatar_for(user, options = { size: 80 })
+        email_address = user.email
+        hash = Digest::MD5.hexdigest(email_address)
+        gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{options[:size]}"
+        image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
+    end
 end
