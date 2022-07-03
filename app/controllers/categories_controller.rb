f9262eeffef8a0120bc74f580d6a0d9ae78d3ff7
thinhnw <thinhnv0412@gmail.com>
2022-07-03 15:19:43 +0000

    Complete category index and show views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,6 +9,7 @@ class CategoriesController < ApplicationController
   end
   def show
     @category = Category.find_by(id: params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def create
